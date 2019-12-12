bc39b96fa9fc91f13a0e119d180607192f301b53
kelvinren <kelvin__ren@hotmail.com>
2019-12-12 15:58:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/useless/UsefulDatabase.kt
@@ -0,0 +1,40 @@
+package com.example.useless
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Useful::class], version = 1)
+
+abstract class UsefulDatabase: RoomDatabase() {
+    abstract fun usefulDao(): UsefulDao
+
+    companion object{
+        //Singelton prevents multiple instances of the database
+        //opening at the same time
+        @Volatile
+        private var INSTANCE : UsefulDatabase? = null
+
+        fun getDatabase(context: Context): UsefulDatabase{
+            val tempDb = INSTANCE
+            if(tempDb!=null){
+                return tempDb
+            }
+
+            //Create an instance of the database
+            synchronized(this){
+                val instance =  Room.databaseBuilder(
+                    context,
+                    UsefulDatabase::class.java,
+                    "useful_db"
+                ).build()
+
+                INSTANCE = instance
+                return instance
+            }
+
+        }
+
+    }
+}
